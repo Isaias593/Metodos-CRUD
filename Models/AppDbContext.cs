e35f984975070c828fc1be32921759062f75c904
Isaias Coronel Zapa <58237470+Isaias593@users.noreply.github.com>
2023-06-27 20:40:12 -0500

    Add files via upload

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAWA_MetodosCRUD.Models
+{
+    public class AppDbContext : DbContext
+    {
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=INCORONELV2;Trusted_Connection=true;Encrypt=false;");
+        }
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DAWA_MetodosCRUD.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool Estado { get; set; }
+    }
+}
